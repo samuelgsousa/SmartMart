411db0b244e8b262abf0d478f6e896b8890435ab
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-28 09:05:12 -0300

    Update database.py
    Tornando compatível com variáveis de ambiente

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:4224@localhost:3306/SmartMart"
+# Carregando variáveis do .env
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
