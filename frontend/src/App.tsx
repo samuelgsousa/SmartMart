1a28454cd24eacff6d0ef2dc8d59f65bc0958a60
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-28 11:48:23 -0300

    Update App.tsx
    Adicionando tabela ao app, e testando o serviço para obter as vendas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,47 @@
 import React from"react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+
+import SalesService from "./services/sales.service"
 
 function App() {
+    const teste = SalesService.getAll()
+    console.log(teste)
+
     return (
-        <p>Esse será o app</p>
+        <>
+        <h1 className="text-3xl font-bold underline">Smart Mart</h1>
+
+        <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+
+      
+        </>
   );
   }
   
