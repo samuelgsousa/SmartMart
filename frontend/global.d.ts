76872ad897a5092b015b443c4f40a2574020c0a1
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-28 11:13:55 -0300

    Corrigindo configurações
    Adicionando configurações para o TS reconhecer corretamente o tailwind

---
+++ frontend/global.d.ts
@@ -0,0 +1,7 @@
+declare module '@tailwindcss/vite' {
+    import type { Plugin } from 'vite'
+    /**
+     * Plugin oficial do Tailwind para Vite
+     */
+    export default function tailwindcss(): Plugin
+  }
