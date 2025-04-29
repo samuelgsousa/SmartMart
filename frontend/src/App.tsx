87e87e882caca6ae6bf3b12487ccc5dab09a969d
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-29 08:51:34 -0300

    Configurando provedor de rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,11 @@
 import React from"react";
-import { useState, useEffect, useMemo  } from "react";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table";
-
-import SalesService from "./services/sales.service" 
-
+import AppRoutes from "./routes/AppRoutes";
 
 
 function App() {
 
-    const [sales, setSales] = useState<any[]>([]);
+    return <AppRoutes />
 
-
-
-
-    useEffect(() => {
-        SalesService.getAll().then(data => {
-          setSales(data);
-        });
-      }, []);
-
-    return (
-        <>
-        <h1 className="text-3xl font-bold underline">Smart Mart</h1>
-
-        <Table>
-            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-            <TableHeader>
-                <TableRow>
-                    <TableHead key={"product_name"}>Product Name</TableHead>
-                    <TableHead key={"total_price"}>Total Price</TableHead>
-                    <TableHead key={"quantity"}>Quantity</TableHead>
-                    <TableHead key={"date"}>Date</TableHead>
-                </TableRow>
-            </TableHeader>
-
-            <TableBody>
-            {sales.map(sale => (
-                <TableRow key={sale.id}>
-                    <TableCell key={"product_name_cell"}>{sale.product_name}</TableCell>
-                    <TableCell key={"total_price_cell"}>{sale.total_price}</TableCell>
-                    <TableCell key={"quantity_cell"}>{sale.quantity}</TableCell>
-                    <TableCell key={"date"}>{new Date(sale.date).toLocaleDateString()}</TableCell>
-                </TableRow>
-            ))}
-            </TableBody>
-        </Table>
-
-      
-        </>
-  );
   }
   
   export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 //import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
-  <React.StrictMode>
-    <App />
-
-  </React.StrictMode>,
+  <BrowserRouter>
+  <App />
+</BrowserRouter>
 )
