2b93b74ff6e11b2c9668e46d9be69fcb4fec5b20
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-29 08:51:49 -0300

    Update Dashboard.tsx
    adicionando dados da dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
 
+import SalesService from "../services/sales.service" 
 
 const Dashboard = () => {
+    const [sales, setSales] = useState<any[]>([]);
+
+
+
+
+    useEffect(() => {
+        SalesService.getAll().then(data => {
+          setSales(data);
+        });
+      }, []);
+
     return (
         <>
-        <p>Dashboard</p>
+        <h1 className="text-3xl font-bold underline">Smart Mart</h1>
+
+        <Table>
+            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+            <TableHeader>
+                <TableRow>
+                    <TableHead key={"product_name"}>Product Name</TableHead>
+                    <TableHead key={"total_price"}>Total Price</TableHead>
+                    <TableHead key={"quantity"}>Quantity</TableHead>
+                    <TableHead key={"date"}>Date</TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+            {sales.map(sale => (
+                <TableRow key={sale.id}>
+                    <TableCell key={"product_name_cell"}>{sale.product_name}</TableCell>
+                    <TableCell key={"total_price_cell"}>{sale.total_price}</TableCell>
+                    <TableCell key={"quantity_cell"}>{sale.quantity}</TableCell>
+                    <TableCell key={"date"}>{new Date(sale.date).toLocaleDateString()}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+
+      
         </>
-    );
+  );
   };
   
 export default Dashboard;
