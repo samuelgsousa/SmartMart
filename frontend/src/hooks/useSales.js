901facb9d1aa3b3d2abab90504b6fa9c2e0edd13
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-29 09:05:25 -0300

    Create useSales.js
    Criando hook para atualizar dados das vendas sem precisar recarregar a página

---
+++ frontend/src/hooks/useSales.js
@@ -0,0 +1,27 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import SalesService from "../services/sales.service" 
+
+export const useSales = () => {
+    //const queryClient = useQueryClient();
+
+    const query = useQuery({
+        queryKey: ['sales'],
+        queryFn: async () => {
+            const response = await SalesService.getAll();
+            if (!response) throw new Error('Erro ao carregar vendas!');
+            return response.data;
+        },
+        // Mantém dados anteriores durante o carregamento
+        keepPreviousData: true,
+        // Configuração de cache
+        staleTime: 60 * 1000 // 1 minuto
+    })
+
+    return {
+        sales: query?.data || [],
+        isLoading: query.isLoading,
+        isFetching: query.isFetching,
+        isError: query.isError,
+        refetch: query.refetch,
+    }
+}
