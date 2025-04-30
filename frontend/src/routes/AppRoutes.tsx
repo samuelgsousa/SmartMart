5ffc3e59459cbb44b9e4cc5790297d4ad18062e2
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-30 14:55:40 -0300

    Update AppRoutes.tsx
    Adicionando página de produtos na rota

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route, Router, Navigate } from 'react-router-dom';
 import Dashboard from '@/pages/Dashboard';
+import Products from '@/pages/Products';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/products" element={<Products />} />
         </Routes>
     )
 }
