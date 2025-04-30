1be7fb5035c59317dba29965786bc19700de3f23
Samuel Sousa <samuel.sousa.rt@gmail.com>
2025-04-30 14:57:40 -0300

    Update Products.tsx
    Verificando se o hook ainda funciona

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,6 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+
+import {
+Dialog,
+DialogContent,
+DialogHeader,
+DialogTitle,
+} from "@/components/ui/dialog"
+
+import {useProducts} from '../hooks/useProducts'
 
 const Products = () => {
+
+    const {products} = useProducts()
+
+    useEffect(() => {
+        if (products) console.log(products)
+    }, [products]);
     return (
         <>
         <p>Página de produtos!</p>
