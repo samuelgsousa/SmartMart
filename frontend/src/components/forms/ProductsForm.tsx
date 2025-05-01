eddd3840366d16420d5da425c1eb7bd04ac37211
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-01 17:09:42 -0300

    Update ProductsForm.tsx
    Adicionando schema de formulário

--- frontend/src/components/forms/ProductsForm.tsx
+++ frontend/src/components/forms/ProductsForm.tsx
@@ -17,6 +17,29 @@ import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover
 import {useProducts} from '../../hooks/useProducts'
 import { Product } from "@/interfaces/interfaces"
 
+const formSchema = z.object({
+    name: z.string({
+        message: "Digite um nome"// Se não for número
+      }),
+
+    price: z.coerce
+    .number({
+      invalid_type_error: "Preço deve ser um número",
+    })
+    .positive("Preço não pode ser negativo"),
+
+    category_id: z.coerce    .number({
+        invalid_type_error: "Selecione uma categoria válida!", // Se não for número
+      })
+      .positive("Selecione uma categoria!")
+      .min(1, "Selecione uma categoria!"),
+
+    brand: z.string({
+        message: "Digite uma marca!"// Se não for número
+      }),
+
+  })
+
 const ProductsForm = ({productUpdating, onSuccess}) => {
 
 
