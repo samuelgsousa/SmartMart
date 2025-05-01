5fcce7d0418d0021bceab44189d49e30a9b858c3
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-01 20:00:32 -0300

    Update ProductsForm.tsx
    Adicionando  outros campos no formulário

--- frontend/src/components/forms/ProductsForm.tsx
+++ frontend/src/components/forms/ProductsForm.tsx
@@ -18,15 +18,15 @@ import {useProducts} from '../../hooks/useProducts'
 import { Product } from "@/interfaces/interfaces"
 
 const formSchema = z.object({
-    name: z.string({
-        message: "Digite um nome"// Se não for número
-      }),
+    name: z.string() // Primeiro verifica se é string
+    .min(1, { message: "O nome não pode estar vazio" }) // Depois valida o comprimento
+    .max(250, { message: "O nome deve ter no máximo 250 caracteres" }),
+
+    description: z.string().optional(),
 
     price: z.coerce
-    .number({
-      invalid_type_error: "Preço deve ser um número",
-    })
-    .positive("Preço não pode ser negativo"),
+    .number({invalid_type_error: "Preço deve ser um número",})
+    .positive("Preço não pode ser negativo ou zero"),
 
     category_id: z.coerce    .number({
         invalid_type_error: "Selecione uma categoria válida!", // Se não for número
@@ -34,13 +34,113 @@ const formSchema = z.object({
       .positive("Selecione uma categoria!")
       .min(1, "Selecione uma categoria!"),
 
-    brand: z.string({
-        message: "Digite uma marca!"// Se não for número
-      }),
+    brand: z.string() 
+    .min(1, { message: "A marca não pode estar vazia" }) 
+    .max(45, { message: "A marca deve ter no máximo 45 caracteres" }),
 
   })
 
 const ProductsForm = ({productUpdating, onSuccess}) => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: productUpdating || {
+            name: '',
+            description: '',
+            price: 0,
+            category_id: 0,
+            brand: ''
+            },
+    })
+
+    const onSubmit = async (data) => {
+        console.log("Dados a serem enviados: ", data)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+            {/* Nome do produto */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                    <FormControl>
+                        <Input type={"text"} placeholder="product name" {...field} />
+                    </FormControl>
+                  <FormMessage />
+                </FormItem>
+                
+              )}
+            />
+
+            {/* Descrição do produto */}
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                    <FormControl>
+                        <Input type={"text"} placeholder="description" {...field} />
+                    </FormControl>
+                  <FormMessage />
+                </FormItem>
+                
+              )}
+            />
+
+            {/* Valor do produto */}
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Priece</FormLabel>
+                  <FormControl>
+                    <Input type={"number"} placeholder="value" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+                
+              )}
+            />
+
+                        {/* Select do produto */}
+          <FormField
+            control={form.control}
+            name="category_id"
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Category</FormLabel>
+                        <Select onValueChange={field.onChange} value={String(field.value)}>
+                            <FormControl>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a category" />
+                                </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                            <SelectItem value="0">Select a category</SelectItem>
+
+                              {/* {products?.map(product => (
+                                <SelectItem key={product.id} value={String(product.id)}>{product.name}</SelectItem>
+                              ))} */}
+
+                            </SelectContent>
+                        </Select>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+
+            <Button type="submit">Submit</Button>
+        </form>
+        </Form>
+    )
 
 
 }
