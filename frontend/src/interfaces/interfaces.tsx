58d03e8f8aa907ac3a03d179315d354203829f38
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-01 16:47:52 -0300

    Update interfaces.tsx
    Adicionando nome da categoria na interface

--- frontend/src/interfaces/interfaces.tsx
+++ frontend/src/interfaces/interfaces.tsx
@@ -1,9 +1,11 @@
 export interface Product {
+
     id: number
     name: string
     description: string
     price: number
     category_id: number
+    category_name: string
     brand: string
   }
 
