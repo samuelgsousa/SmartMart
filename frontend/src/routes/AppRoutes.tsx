604381954afd6dd1f66538e53f7efb89d868b183
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-04 17:45:30 -0300

    Update AppRoutes.tsx
    Criando rota para vendas

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Router, Navigate } from 'react-router-dom';
 import Dashboard from '@/pages/Dashboard';
 import Products from '@/pages/Products';
+import Sales from '@/pages/Sales';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/sales" element={<Sales />} />
         </Routes>
     )
 }
