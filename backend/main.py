5a9ed4302fa8ecde59cc0a1902147de05b19cee1
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-03 21:38:09 -0300

    Update main.py
    Adicionando configurações esseciais

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
+    "http://localhost:8000"
     # Depois adicionar outros domínios/deploys aqui:
     # Ex: "https://meu-app.vercel.app",
 ]
@@ -26,6 +27,7 @@ app.add_middleware(
     allow_credentials=True,        
     allow_methods=["*"],    
     allow_headers=["*"],
+    expose_headers=["*"] 
 )
 
 
