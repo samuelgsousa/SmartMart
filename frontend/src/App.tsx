cddf60e29bf64ac8dcc38d6540790fb9cb3feec1
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-04 17:32:26 -0300

    Adicionando sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from"react";
 import AppRoutes from "./routes/AppRoutes";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Sidebar, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { Link } from "react-router-dom";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,8 +18,23 @@ function App() {
 
     return(
     <QueryClientProvider client={queryClient}>
-       <h1 className="text-3xl font-bold underline">Smart Mart</h1>
-      <AppRoutes />
+      <SidebarProvider>
+        <main>
+        <h1 className="text-3xl font-bold underline">Smart Mart</h1>
+          <SidebarTrigger />
+
+          <Sidebar>
+          <SidebarTrigger />
+          <Link to="/">Dashboard</Link>
+          <Link to="/products">Products</Link>
+
+          
+          </Sidebar>
+
+        <AppRoutes />
+        </main>
+      </SidebarProvider>
+
     </QueryClientProvider>
     )
 
