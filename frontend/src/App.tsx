07c1ab7d7d594c5907f75ae436b83025c01e7307
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-04 17:45:54 -0300

    Update App.tsx
    Adicionando sidebar para transição entre as páginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from"react";
 import AppRoutes from "./routes/AppRoutes";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Sidebar, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { Link } from "react-router-dom";
+import { BadgeDollarSign, House, Package } from "lucide-react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,17 +20,41 @@ function App() {
     return(
     <QueryClientProvider client={queryClient}>
       <SidebarProvider>
-        <main>
+        <main className="w-full min-h-screen">
         <h1 className="text-3xl font-bold underline">Smart Mart</h1>
           <SidebarTrigger />
 
-          <Sidebar>
-          <SidebarTrigger />
-          <Link to="/">Dashboard</Link>
-          <Link to="/products">Products</Link>
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+        
+          <SidebarGroupLabel>  Application <SidebarTrigger /></SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+            
+                <SidebarMenuItem key={"dashboard_item"}>
+                  <SidebarMenuButton asChild>
+                    <Link to="/"> <House /> <span>Dashboard</span></Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
 
-          
-          </Sidebar>
+                <SidebarMenuItem key={"product_item"}>
+                  <SidebarMenuButton asChild>
+                    <Link to="/products"> <Package /> <span>Products</span></Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+
+                <SidebarMenuItem key={"sales_item"}>
+                  <SidebarMenuButton asChild>
+                    <Link to="/sales"> <BadgeDollarSign /> <span>Sales</span></Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
 
         <AppRoutes />
         </main>
