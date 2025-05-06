cb139da19954bffff76fa0fc285b0031a9496e56
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2025-05-05 21:09:38 -0300

    Update httpClient.js

--- frontend/src/services/httpClient.js
+++ frontend/src/services/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
     withCredentials: true
   });
